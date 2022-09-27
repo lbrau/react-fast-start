96955dac37b6ab4a34f61f48bdb12c8ae4e510e4
Laurent <laurent.brau@gmail.com>
2022-09-27 23:02:22 +0200

    add styled component examples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
+import React, { useState, useEffect, Fragment } from "react";
 import './App.css';
+import SuccessButton, { DropButton, WarnButton, InfoButton } from './Elements/StyledButton';
 
 function App() {
+  const DISPLAY_LABEL = "Cacher les secrets";
+  const HIDDEN_LABEL = "Afficher les secrets";
+
+  const [state, setState] = useState({
+    count: 0,
+    visible: true,
+    label: DISPLAY_LABEL
+  });
+
+  useEffect(() => {
+    console.log('application effect');
+  }, [state]);
+
+  const addToCounter = () => {
+    const newCount = state.count + 1;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        count: newCount
+      }
+    })
+  };
+
+  const dropToCounter = () => {
+    const newCount = state.count - 1;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        count: newCount
+      }
+    });
+  };
+
+  const toggleVisibility = () => {
+    setState(prevState => {
+      return {
+        ...prevState,
+        visible: !state.visible,
+        label: state.visible ?  HIDDEN_LABEL : DISPLAY_LABEL 
+      }
+    })
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.visible ?  
+        <Fragment>
+          <h3>{state.count}</h3>
+          <SuccessButton onClick={addToCounter}>ajouter</SuccessButton>
+          <WarnButton onClick={dropToCounter}>retirer</WarnButton>
+          <InfoButton onClick={toggleVisibility}>{state.label}</InfoButton>
+        </Fragment>
+       : <Fragment>
+          <p>Rien à afficher</p>
+          <DropButton onClick={toggleVisibility}>{state.label}</DropButton>
+        </Fragment>}
+        
     </div>
   );
 }
---
+++ src/Elements/StyledButton.js
@@ -0,0 +1,38 @@
+import Styled from "styled-components";
+
+const SuccessButton = Styled.button`
+    background: green;
+    color: white;
+    height: 30px;
+    padding: 5px 10px;
+    font-size: 14px;
+    font-weight: bold;
+`
+
+export const InfoButton = Styled.button`
+    background: blue;
+    color: white;
+    height: 30px;
+    padding: 5px 10px;
+    font-size: 14px;
+    font-weight: bold;
+`
+export const DropButton = Styled.button`
+    background: red;
+    color: white;
+    height: 30px;
+    padding: 5px 10px;
+    font-size: 14px;
+    font-weight: bold;
+`
+
+export const WarnButton = Styled.button`
+    background: orange;
+    color: white;
+    height: 30px;
+    padding: 5px 10px;
+    font-size: 14px;
+    font-weight: bold;
+`
+
+export default SuccessButton;
